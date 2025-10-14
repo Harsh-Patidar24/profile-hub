5a49d35e934bff09edff3d09c216b3ac665b761c
Harsh Patidar <harshpatidar242004@gmail.com>
2025-10-14 17:51:58 +0530

    fds

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,11 +2,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "https://profile-hub-lah6.onrender.com" : "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL === "production" ? "https://profile-hub-lah6.onrender.com" : "http://localhost:5000",
 });
 
 api.interceptors.request.use((config) => {
-  console.log("ENV:", process.env.NEXT_PUBLIC_ENVIRONMENT);
+  console.log("ENV:", process.env.NEXT_PUBLIC_API_URL);
   const token = localStorage.getItem("token");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
