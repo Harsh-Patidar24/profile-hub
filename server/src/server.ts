9fe351cfdbdbab6f31673465827632eea78b8601
Harsh Patidar <harshpatidar242004@gmail.com>
2025-10-14 18:19:28 +0530

    backend server chnages

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin:[ "http://localhost:3000", "https://profile-hub-two.vercel.app"], credentials: true }));
 app.use(express.json());
 
 connectDB();
